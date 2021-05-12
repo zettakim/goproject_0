c6dc934b7360942c66da797e6f23e128cd6707ff
Zetta Kim <zetta.kim@icloud.com>
2021-05-12 21:37:15 +0900

    5/12 commit

---
+++ ch06/ex6.6/ex6.8.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const epsilon = 0.000001
+
+func equal(a, b float64) bool {
+	return math.Nextafter(a, b) == b
+}
+
+func main() {
+	var a float64 = 0.1
+	var b float64 = 0.2
+	var c float64 = 0.3
+
+	fmt.Printf("%0.18f + %0.18f = %0.18f\n", a, b, a+b)
+	fmt.Printf("%0.18f == %0.18f : %v\n", c, a+b, equal(a+b, c))
+
+	a = 0.0000000000004
+	b = 0.0000000000002
+	c = 0.0000000000007
+
+	fmt.Printf("%g == %g : %v\n", c, a+b, equal(a+b, c))
+}
