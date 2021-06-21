fbc4c429f6e04a8b42185b48d3463918a702a129
Zetta Kim <zetta.kim@icloud.com>
2021-06-21 13:07:58 +0900

    ex17.9 - 1 commit

---
+++ ch17/ex17.9/ex17.9.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+const (
+	Balance		= 1000
+	EarnPoint	= 500
+	LosePoint = 100
+	VictoryPoint = 5000
+	GameoverPoint = 0
+)
+
+var stdin = bufio.NewReader(os.Stdin)`
+
+func main() {
+}
